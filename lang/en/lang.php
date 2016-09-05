444ceaa5fbcb1e3096cad237c8369f55e95c9fc2
Green It Solutions <manu@greenitsolutions.fr>
2016-09-05 08:38:38 +0200

    Add files via upload

---
+++ lang/en/lang.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * English language file
+ *
+ * @author J. Ferre
+ */
+
+$lang['panel_24rj']  = "Insert a panel 24 ports with no name";
+$lang['panel_1x24']  = "Insert a panel 24 ports with name";
+$lang['panel_1x48']  = "Insert a panel 48 ports with name";
+$lang['panel_2x24']  = "Insert a panel 48 ports with two names";
+$lang['panel_2x12']  = "Insert a panel 2x12 ports with name";
+$lang['panel_4x12']  = "Insert a panel 4x12 ports with two names";
+$lang['panel_logo']  = "Insert a panel 24 ports with logo";
+$lang['panel_ears']  = "Insert a panel 24 ports with fixings";
+$lang['panel_info']  = "Documentation and bug reports: https://github.com/GreenItSolutions/dokuwiki-plugin-switchpanel";
+$lang['name_1x24a']  = "24 ports on 1U (01-24)";
+$lang['name_1x48a']  = "48 ports on 2U (01-48)";
+$lang['name_2x24a']  = "48 ports on 2U (01-24)";
+$lang['name_2x24b']  = "48 ports on 2U (25-48)";
+$lang['name_2x12a']  = "2x12 ports on 1U (01-12 & 13-24)";
+$lang['name_4x12a']  = "4x12 ports on 2U (01-12 & 13-24)";
+$lang['name_4x12b']  = "4x12 ports on 2U (25-36 & 37-48)";
+$lang['name_logo']   = "Panel with logo";
+$lang['name_ears']   = "Panel with fixings without logo";
+$lang['rj']          = "RJ45 port";
+$lang['of']          = "OF port";
+$lang['2of']         = "2xOF port";
+$lang['gbic']        = "Gbic port";
+$lang['serial']      = "Serial port";
+$lang['close']       = "Closed port";
+$lang['none']        = "No port";
+$lang['syntax']      = "List of options for a port";
+$lang['port_of']     = "Optical Fiber";
+$lang['port_2of']    = "Dual Fiber";
+$lang['port_gbic']   = "Gbic port";
+$lang['port_close']  = "Closed";
+$lang['port_serial'] = "Serial port";
+$lang['port_title']  = "Info";
+$lang['port_url']    = "http://link.address";
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/fr/lang.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * French language file
+ *
+ * @author J. Ferre
+ */
+
+$lang['panel_24rj']  = "Insérer un bandeau 24 ports sans nom";
+$lang['panel_1x24']  = "Insérer un bandeau 24 ports avec nom";
+$lang['panel_1x48']  = "Insérer un bandeau 48 ports avec nom";
+$lang['panel_2x24']  = "Insérer un bandeau 48 ports avec deux noms";
+$lang['panel_2x12']  = "Insérer un bandeau 2x12 ports avec nom";
+$lang['panel_4x12']  = "Insérer un bandeau 4x12 ports avec deux noms";
+$lang['panel_logo']  = "Insérer un bandeau 24 ports avec logo";
+$lang['panel_ears']  = "Insérer un bandeau 24 ports avec fixations";
+$lang['panel_info']  = "Documentation et rapports de bug : https://github.com/GreenItSolutions/dokuwiki-plugin-switchpanel";
+$lang['name_1x24a']  = "24 ports sur 1U (01-24)";
+$lang['name_1x48a']  = "48 ports sur 2U (01-48)";
+$lang['name_2x24a']  = "48 ports sur 2U (01-24)";
+$lang['name_2x24b']  = "48 ports sur 2U (25-48)";
+$lang['name_2x12a']  = "2x12 ports sur 1U (01-12 & 13-24)";
+$lang['name_4x12a']  = "4x12 ports sur 2U (01-12 & 13-24)";
+$lang['name_4x12b']  = "4x12 ports sur 2U (25-36 & 37-48)";
+$lang['name_logo']   = "Bandeau avec logo";
+$lang['name_ears']   = "Bandeau avec fixations sans logo";
+$lang['rj']          = "Port RJ45";
+$lang['of']          = "Port FO";
+$lang['2of']         = "Port 2xFO";
+$lang['gbic']        = "Port Gbic";
+$lang['serial']      = "Port série";
+$lang['close']       = "Port fermé";
+$lang['none']        = "Aucun port";
+$lang['syntax']      = "Liste des options pour un port";
+$lang['port_of']     = "Fibre optique";
+$lang['port_2of']    = "Fibre double";
+$lang['port_gbic']   = "Port Gbic";
+$lang['port_close']  = "Fermé";
+$lang['port_serial'] = "Port série";
+$lang['port_title']  = "Information supplémentaire";
+$lang['port_url']    = "http://adresse.du.lien";
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
